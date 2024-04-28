a13dc807c93fe56d117096d2a86ae99adf080061
birajregmi7 <biraj.regmi.jr@gmail.com>
2024-04-29 00:37:30 +0545

    some updates on css

--- youtube-proj/src/components/CardVideo.jsx
+++ youtube-proj/src/components/CardVideo.jsx
@@ -5,15 +5,19 @@ import { toggleVideoPage } from "../utils/toggleSlice";
 const CardVideo = (youtubeData) => {
   const dispatch = useDispatch();
   const videoPageToggle = () => {
-    console.log('Going to dispatch 2')
+    console.log("Going to dispatch 2");
     dispatch(toggleVideoPage());
   };
   return (
     <div className="flex flex-wrap">
-      {youtubeData.details.map((x) => {
+      {youtubeData.details.map((x, idx) => {
         return (
           <>
-            <Link onClick={() => videoPageToggle()} to={"/watch?v=" + x.id}>
+            <Link
+              key={idx}
+              onClick={() => videoPageToggle()}
+              to={"/watch?v=" + x.id}
+            >
               <div className="flex flex-col w-72 m-3">
                 <img
                   className="rounded-xl"
@@ -22,13 +26,15 @@ const CardVideo = (youtubeData) => {
                 />
                 <div className="font-medium">{x?.snippet?.title}</div>
                 <div>{x?.snippet?.channelTitle}</div>
-                <div>{x?.statistics?.viewCount.split().map((x)=> {
-                  if(x.length<=6){
-                    return `${x[0]}${x[1]}K`
-                  }else if(x.length==7){
-                    return  `${x[0]}${x[1]}M`
-                  }
-                })}</div>
+                <div>
+                  {x?.statistics?.viewCount.split().map((x) => {
+                    if (x.length <= 6) {
+                      return `${x[0]}${x[1]}K`;
+                    } else if (x.length == 7) {
+                      return `${x[0]}${x[1]}M`;
+                    }
+                  })}
+                </div>
               </div>
             </Link>
           </>
--- youtube-proj/tailwind.config.js
+++ youtube-proj/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'Arial', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
